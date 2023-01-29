020a384df3800eb4e554c3574b595ecdc234bdd2
Drew Conner <drew@blinkingboxes.net>
2023-01-29 11:07:29 -0600

    cleaned up a few unneeded comments, changed names of some globals, fixed some spacing issues, and added .gitignore; overall some trivial/superficial but needed changes

--- ipcalc.py
+++ ipcalc.py
@@ -1,12 +1,14 @@
 #!/bin/python3
+
 import sys
+
 # network calculator based on 2 inputs: 
 # ipv4 address and subnet mask, both in
 # dotted-decimal format
 
 
 # initializing the dictionary of all input and output values
-ip_dict = {
+NETWORK_DATA_DICT = {
     'ip_add': [0, 'Host IPv4 Address'],
     'sub_mask': [0, 'Subnet Mask'],
     'wildcard': [0, 'Wildcard Mask'],
@@ -14,17 +16,11 @@ ip_dict = {
     'bcast': [0, 'Broadcast Address'],
     'hosts': [0, 'Host addresses']
 }
-inp_keys = ['ip_add', 'sub_mask']
-outp_keys = ['wildcard', 'network', 'bcast', 'hosts']
+INPUT_KEYS = ['ip_add', 'sub_mask']
+OUTPUT_KEYS = ['wildcard', 'network', 'bcast', 'hosts']
 
-
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# BEGIN HELPER FUNCTIONS
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 def checkIP(address):
-    # function definition incomplete
     '''
     --checks 4-item list as ipv4 address--
     input is dotted-decimal ipv4 address as string
@@ -52,7 +48,6 @@ def checkIP(address):
     return True
         
 def checkSubMask(address):
-    # function definition incomplete
     '''
     checks 4-item list as ipv4 address
     and returns True if it is a valid
@@ -85,7 +80,6 @@ def checkSubMask(address):
 
 
 def ipv4ToBin(address):
-    # function definition complete
     '''
     takes in dotted-decimal ipv4 address 
     # takes in 4-item list as
@@ -106,7 +100,6 @@ def ipv4ToBin(address):
 
 
 def binToIPv4(address):
-    # function definition incomplete
     '''
     takes in 32-bit unsigned integer and
     returns dotted-decimal string
@@ -124,7 +117,6 @@ def binToIPv4(address):
 
 
 def get_wildcard(mask):
-    # function definition incomplete
     '''
     calculates wildcard mask based on subnet mask
     subnet mask input as string
@@ -141,7 +133,6 @@ def get_wildcard(mask):
 
 
 def get_network(address, mask):
-    # function definition complete
     '''
     input of dotted-decimal string representations
     of a host address, return a dotted-decimal
@@ -159,7 +150,6 @@ def get_network(address, mask):
 
 
 def get_broadcast(address, mask):
-    # function definition complete
     '''
     input of dotted-decimal string representations
     of a host address, return a dotted-decimal
@@ -177,7 +167,6 @@ def get_broadcast(address, mask):
 
 
 def num_hosts(mask):
-    # function definition complete
     '''
     takes in dotted-decimal subnet mask
     returns number of hosts the subnet supports
@@ -192,9 +181,11 @@ def num_hosts(mask):
 
 
 def convert_mask(prefix_length):
-    #function definition incomplete
-    #take in integer value between 0-32, immediately return false if not
-    #output tuple of 2 strings: (dotted-decimal mask, binary mask string)
+    """
+    take in integer value between 0-32, immediately return false if not
+    
+    output tuple of 2 strings: (dotted-decimal mask, binary mask string)
+    """
     prefix_length = int(prefix_length)
     if prefix_length in range(33):
         #do the conversion
@@ -208,8 +199,7 @@ def convert_mask(prefix_length):
 
 
 
-def get_input():
-    #function definition complete
+def get_input(None) -> None:
     '''
     takes in ipv4 address and subnet mask,
     checks for valid input
@@ -233,64 +223,39 @@ def get_input():
     return (str(ip_info[0]), str(ip_info[1]))
 
 
-
-def print_output():
+def print_output(None) -> None:
     table_len = 40
     print("\n\n" + "="*table_len)
     print("| Input Data: " + ' '*(table_len-15) + '|')
     print('| ' + '-'*(table_len-4) + ' |')
-    for key in inp_keys:
-        data_len = len(ip_dict[key][1]) + len(ip_dict[key][0])
-        print('| ' + str(ip_dict[key][1]) + ' '*(table_len-data_len-4) + str(ip_dict[key][0]) + ' |')
+    for key in INPUT_KEYS:
+        data_len = len(NETWORK_DATA_DICT[key][1]) + len(NETWORK_DATA_DICT[key][0])
+        print('| ' + str(NETWORK_DATA_DICT[key][1]) + ' '*(table_len-data_len-4) + str(NETWORK_DATA_DICT[key][0]) + ' |')
     print('='*table_len + '\n| Output Data:' + ' '*(table_len-15) +  '|')
     print('| ' + '-'*(table_len-4) + ' |')
-    for key in outp_keys:
-        data_len = len(str(ip_dict[key][1])) + len(str(ip_dict[key][0]))
-        print('| ' + str(ip_dict[key][1]) + ' '*(table_len-data_len-4) + str(ip_dict[key][0]) + ' |')
+    for key in OUTPUT_KEYS:
+        data_len = len(str(NETWORK_DATA_DICT[key][1])) + len(str(NETWORK_DATA_DICT[key][0]))
+        print('| ' + str(NETWORK_DATA_DICT[key][1]) + ' '*(table_len-data_len-4) + str(NETWORK_DATA_DICT[key][0]) + ' |')
     print('='*table_len + '\n\n')
 
 
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# END HELPER FUNCTIONS
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-
-
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# begin main()
 def main():
-    # GET USER INPUT
-    # get_input returns 2-item tuple of (address, mask)
     inp_tup = get_input()
     if inp_tup == 0:
         print('There was a problem with your input, program will now exit.')
         return 0
         
     net_host, net_mask = inp_tup[0], inp_tup[1]
-    # add inputs to dictionary
-    for key in range(len(inp_keys)):
-        ip_dict[inp_keys[key]][0] = inp_tup[key]
-        # ip_dict[inp_keys[key]][1] = ipv4ToBin(ip_dict[inp_keys[key]][0])
+    for key in range(len(INPUT_KEYS)):
+        NETWORK_DATA_DICT[INPUT_KEYS[key]][0] = inp_tup[key]
+    NETWORK_DATA_DICT['wildcard'][0] = get_wildcard(net_mask)
+    NETWORK_DATA_DICT['network'][0] = get_network(net_host, net_mask)
+    NETWORK_DATA_DICT['bcast'][0] = get_broadcast(net_host, net_mask)
+    NETWORK_DATA_DICT['hosts'][0] = num_hosts(net_mask)
 
-    # use inputs to add output data to dictionary
-    # ref: outp_keys = ['wildcard', 'network', 'bcast', 'hosts']
-    ip_dict['wildcard'][0] = get_wildcard(net_mask)
-    ip_dict['network'][0] = get_network(net_host, net_mask)
-    ip_dict['bcast'][0] = get_broadcast(net_host, net_mask)
-    ip_dict['hosts'][0] = num_hosts(net_mask)
-
-    # temp output to see all values in ip_dict
-    # print("\n\n" + "#"*40)
-    # for key in ip_dict.keys():
-    #   print(str(ip_dict[key][1]) + ': ' + str(ip_dict[key][0]))
-
-
-    # final: print dictionary and dotted-decimal repr of addresses/masks
     print_output()
     return None
-# end main()
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-main()
+
+if __name__ == "__main__":
+    main()
